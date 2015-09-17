179af6af237f08801549537c536d7e34456ee20a
RihaGupta <rihagupta5992@gmail.com>
2015-09-17 02:03:12 -0700

    Update unittest1.go

--- unittest1.go
+++ unittest1.go
@@ -1,26 +1,26 @@
 package main
 import "testing"
 
-type rectest struct{
+type test1 struct{
 	recs Rectangle
 	result float64
 }
-type cirtest struct{
+type test2 struct{
 	cirs Circle
 	result float64
 }
 
-var rectests = []rectest{{Rectangle{0,0,2,2},8},{Rectangle{0,0,4,4},16}}
-var cirtests = []cirtest{{Circle{0,0,4},25.132741228718345}}
+var rec = []test1{{Rectangle{2,2},8},{Rectangle{4,4},16}}
+var cir = []test2{{Circle{4},25.132741228718345}}
 
 func TestPerimeter(t *testing.T){
-	for _,cases:= range rectests{
+	for _,cases:= range rec{
 		v1:=cases.recs.Peri()
 		if v1!=cases.result{
 			t.Error("Expected",cases.result, "got",v1)
 		}
 	}
-	for _,cases:= range cirtests{
+	for _,cases:= range cir{
 		v1:=cases.cirs.Peri()
 		if v1!=cases.result{
 			t.Error("Expected",cases.result, "got",v1)
